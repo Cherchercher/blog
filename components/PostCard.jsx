debc74ae70b1b572cca6587b9c37b98ba153db39
Cher Huang <xiaoxuah@uci.edu>
2023-01-03 00:35:17 -0800

    initial commit

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,7 +5,9 @@ import Link from 'next/link';
 
 import { grpahCMSImageLoader } from '../util';
 
-const PostCard = ({ post }) => (
+const PostCard = ({ post }) => {
+  console.log(post.author);
+  return(
   <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
@@ -53,6 +55,6 @@ const PostCard = ({ post }) => (
       </Link>
     </div>
   </div>
-);
+)};
 
 export default PostCard;
