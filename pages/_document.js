6cff7cdb67d5d2d61c458ac623359210a7a346e5
Cher Huang <xiaoxuah@uci.edu>
2023-01-03 02:44:49 -0800

    mailchimp integration

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            id="mcjs"
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              // eslint-disable-next-line max-len
+              __html: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e957837febcb93fc8cc489fe5/f93b44946bc2e00d26c1a3ba5.js");',
+            }}
+          />
           {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
         </Head>
         <body>
